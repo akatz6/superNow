a65abeafa4555f7144215a51a9da1374b7969cd9
Aaron Katz <aaronkatz@MacBook-Pro.lan>
2022-01-16 07:40:03 -0800

    no longer need user stroe

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from "vuex";
-import user from "./modules/user";
 import data from "./modules/data";
 
 export default createStore({
   modules: {
-    user,
     data,
   },
 });
